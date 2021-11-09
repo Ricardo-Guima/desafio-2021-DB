1b46ff607ebc660f256db823ab6bb7d81bf34a75
Ricardo-Guima <rickguimaraes1976@gmail.com>
2021-11-09 17:07:11 -0300

    2 files modified

--- src/app/cep-list/cep-list.component.ts
+++ src/app/cep-list/cep-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Ceps, ceps } from '../ceps';
+
+import { ViaCepService } from '../via-cep.service';
 
 @Component({
-  selector: 'app-cep-list',
-  templateUrl: './cep-list.component.html',
-  styleUrls: ['./cep-list.component.css']
+  selector: 'app-cep',
+  templateUrl: './cep.component.html',
+  styleUrls: ['./cep.component.css'],
 })
-export class CepListComponent implements OnInit {
+export class CepComponent implements OnInit {
+  cep: any | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private viaCep: ViaCepService) {}
 
   ngOnInit() {
-  }
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const numeroCepFromRoute = String(routeParams.get('numeroCep'));
 
+    // Find the product that correspond with the id provided in route.
+    //this.cep = ceps.find((cep) => cep.cep === numeroCepFromRoute);
+    this.viaCep.getCep(numeroCepFromRoute).subscribe((cep) => (this.cep = cep));
+  }
 }
